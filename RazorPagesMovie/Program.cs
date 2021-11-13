00ed5d60fc66e3187490d24003945e17b773f99d
Johannes Boullay <skywalker3173@outlook.fr>
2021-11-13 10:07:40 -0500

    WTF

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+#Comments
 namespace RazorPagesMovie
 {
     public class Program
