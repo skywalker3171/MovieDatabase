c814d5ea8f6512b77873eaa675a892999be98d5f
Johannes Boullay <skywalker3171@hotmail.com>
2022-04-24 15:38:44 -0400

    Update Program.cs

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-#Comments
 namespace RazorPagesMovie
 {
     public class Program
